014122d5ae0cd4b2f20c6014397cdd610c239f89
sinan <haxsmith77@gmail.com>
2023-04-04 11:46:11 +1000

    Updated main

--- ping/__main__.py
+++ ping/__main__.py
@@ -1,3 +1,12 @@
 from checker import site_is_online
+from cli import read_user_cli_args, display_check_result
 
-site_is_online()
+if __name__ == '__main__':
+    args = read_user_cli_args()
+    urls = args.urls
+    for url in urls:
+        try:
+            is_online = site_is_online(url=url, timeout=3)
+            display_check_result(is_online, url)
+        except Exception as e:
+            display_check_result(False, url, str(e))
